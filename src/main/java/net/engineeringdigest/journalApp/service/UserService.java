e0fa4cca193219d0c7f779e74bdc90f7a6777be5
Sarvadnya Yeole <yeolesv1012@gmail.com>
2026-01-11 18:56:18 +0530

    Created CustomUserDetailsService

--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -4,13 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.UserEntity;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-@Component
+@Service
 public class UserService {
 
     @Autowired
